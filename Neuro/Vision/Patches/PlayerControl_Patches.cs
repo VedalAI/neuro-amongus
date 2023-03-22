2cc2197e490fea7da416dc4279265570b7a2b97a
AlexejheroYTB <32238504+AlexejheroYTB@users.noreply.github.com>
2023-03-23 00:27:11 +0200

    Add vision handler

---
+++ Neuro/Vision/Patches/PlayerControl_Patches.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+using Reactor.Utilities;
+
+namespace Neuro.Vision.Patches;
+
+[HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.Start))]
+public static class PlayerControl_Start
+{
+    public static void Postfix(PlayerControl __instance)
+    {
+        PluginSingleton<NeuroPlugin>.Instance.MainContext.VisionHandler.StartTrackingPlayer(__instance);
+    }
+}
