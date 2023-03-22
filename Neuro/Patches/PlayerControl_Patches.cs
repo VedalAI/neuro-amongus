62112ba9c9ca4b1831d220a18393a924eeb033f6
AlexejheroYTB <32238504+AlexejheroYTB@users.noreply.github.com>
2023-03-22 15:50:46 +0200

    Cleanup NeuroPlugin.FixedUpdate
    - Removed localPlayer parameter from NeuroPlugin.FixedUpdate
    - Get if sabotage is active using PlayerTask.TaskIsEmergency
    - Remove redundant ConfigName and Harmony properties and reduntant null check for PlayerControl.LocalPlayer.myTasks

--- Neuro/Patches/PlayerControl_Patches.cs
+++ Neuro/Patches/PlayerControl_Patches.cs
@@ -1,6 +1,5 @@
 ﻿using HarmonyLib;
 using Reactor.Utilities;
-using UnityEngine;
 
 namespace Neuro.Patches;
 
@@ -18,29 +17,6 @@ public static class PlayerControlFixedUpdate
 {
     public static void Postfix(PlayerControl __instance)
     {
-        if (PlayerControl.LocalPlayer == __instance) PluginSingleton<NeuroPlugin>.Instance.FixedUpdate(__instance);
-    }
-}
-
-[HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.Die))]
-public static class PlayerControl_Die
-{
-    public static void Postfix(DeathReason reason, PlayerControl __instance)
-    {
-        // Check if player was murdered
-        if (reason == DeathReason.Kill)
-        {
-            // Find dead body at death position
-            Collider2D[] deathPointColliders = Physics2D.OverlapCircleAll(__instance.GetTruePosition(), 1f);
-            foreach (Collider2D col in deathPointColliders)
-            {
-                // Call vision method for handling new dead bodies
-                DeadBody deadBody = col.gameObject.GetComponent<DeadBody>();
-                if (deadBody)
-                {
-                    PluginSingleton<NeuroPlugin>.Instance.vision.DeadBodyAppeared(deadBody);
-                }
-            }
-        }
+        if (__instance.AmOwner) PluginSingleton<NeuroPlugin>.Instance.FixedUpdate();
     }
 }
