6220d504a75c183a78ea0c7369c8bc17e8d6922d
oleg20111511 <oleg20111511@list.ru>
2023-03-22 23:38:46 +0200

    Remove FindObjectsOfType for finding DeadBody
    Instead finds a dead body collider at the place of death every time a player dies, and adds that to deadBodies list

--- Neuro/Patches/PlayerControl_Patches.cs
+++ Neuro/Patches/PlayerControl_Patches.cs
@@ -1,5 +1,6 @@
 ﻿using HarmonyLib;
 using Reactor.Utilities;
+using UnityEngine;
 
 namespace Neuro.Patches;
 
@@ -18,5 +19,28 @@ public static class PlayerControlFixedUpdate
     public static void Postfix(PlayerControl __instance)
     {
         if (PlayerControl.LocalPlayer == __instance) PluginSingleton<NeuroPlugin>.Instance.FixedUpdate(__instance);
+    }
+}
+
+[HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.Die))]
+public static class PlayerControl_Die
+{
+    public static void Postfix(DeathReason reason, PlayerControl __instance)
+    {
+        // Check if player was murdered
+        if (reason == DeathReason.Kill)
+        {
+            // Find dead body at death position
+            Collider2D[] deathPointColliders = Physics2D.OverlapCircleAll(__instance.GetTruePosition(), 1f);
+            foreach (Collider2D col in deathPointColliders)
+            {
+                // Call vision method for handling new dead bodies
+                DeadBody deadBody = col.gameObject.GetComponent<DeadBody>();
+                if (deadBody)
+                {
+                    PluginSingleton<NeuroPlugin>.Instance.vision.DeadBodyAppeared(deadBody);
+                }
+            }
+        }
     }
 }
