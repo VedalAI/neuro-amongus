14af4ba96a20d3f834ae8a4df3ac8690ba96b0a2
Vedal <vedal@vedal.xyz>
2023-03-22 01:54:12 +0000

    Clean

--- Neuro/Utils/Methods.cs
+++ Neuro/Utils/Methods.cs
@@ -1,9 +1,4 @@
-﻿using Reactor.Utilities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 namespace Neuro.Utils;
