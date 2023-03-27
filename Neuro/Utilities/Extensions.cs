0291e454415f462c3fd76b43756b82d2baaba848
js6pak <kubastaron@hotmail.com>
2023-03-27 21:26:17 +0200

    Make Extensions.At free

--- Neuro/Utilities/Extensions.cs
+++ Neuro/Utilities/Extensions.cs
@@ -1,11 +1,20 @@
-﻿namespace Neuro.Utilities;
+﻿using System;
+
+namespace Neuro.Utilities;
 
 public static class Extensions
 {
     /// <summary>
     /// This returns the element at the specified index from the il2cpp list.
-    /// JetBrains Rider will complain about an ambiguous indexer if used normally (list[i]), so we call .ToArray on the list first.
-    /// This does produce additional allocations, but the impact is negligible.
+    /// JetBrains Rider will complain about an ambiguous indexer if used normally (list[i]), so we inline it's indexer.
     /// </summary>
-    public static T At<T>(this Il2CppSystem.Collections.Generic.List<T> list, int index) => list.ToArray()[index]; // TODO: Use reflection to get the indexer
+    public static T At<T>(this Il2CppSystem.Collections.Generic.List<T> list, int index)
+    {
+        if ((uint)index >= (uint)list._size)
+        {
+            throw new ArgumentOutOfRangeException(nameof(index), "Index was out of range. Must be non-negative and less than the size of the collection.");
+        }
+
+        return list._items[index];
+    }
 }
