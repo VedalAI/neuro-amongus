85834f38c3cb42f596162cb2865b4e3b3bf194ed
Ethan <ebrogames@gmail.com>
2023-04-02 02:04:10 -0400

    Create ReactorMeltdownSolver.cs

---
+++ Neuro/Minigames/Completion/Solvers/ReactorMeltdownSolver.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using Neuro.Cursor;
+using UnityEngine;
+
+namespace Neuro.Minigames.Completion.Solvers;
+
+[MinigameSolver(typeof(ReactorMinigame))]
+public sealed class ReactorMeltdownSolver : MinigameSolver<ReactorMinigame>
+{
+    public override IEnumerator CompleteMinigame(ReactorMinigame minigame, NormalPlayerTask task)
+    {
+        yield return InGameCursor.Instance.CoMoveTo(minigame.hand);
+        // sit and wait for someone to hold the other button
+        while (minigame.reactor.IsActive)
+        {
+            minigame.isButtonDown = true;
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
