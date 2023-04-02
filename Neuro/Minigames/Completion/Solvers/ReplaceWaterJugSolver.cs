76084d2bacc7b158a5baeeef526e738e64ce4e7b
Ethan <ebrogames@gmail.com>
2023-04-02 01:30:26 -0400

    Create ReplaceWaterJugSolver.cs

---
+++ Neuro/Minigames/Completion/Solvers/ReplaceWaterJugSolver.cs
@@ -0,0 +1,20 @@
+﻿using Neuro.Cursor;
+using System.Collections;
+using UnityEngine;
+
+namespace Neuro.Minigames.Completion.Solvers;
+
+[MinigameSolver(typeof(WaterStage))]
+public sealed class ReplaceWaterJugSolver : MinigameSolver<WaterStage>
+{
+    public override IEnumerator CompleteMinigame(WaterStage minigame, NormalPlayerTask task)
+    {
+        yield return InGameCursor.Instance.CoMoveTo(minigame.waterButton.transform.position);
+        minigame.Refuel();
+        while (!minigame.complete)
+            yield return new WaitForFixedUpdate();
+        minigame.Close();
+    }
+}
+
+
