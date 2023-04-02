2a14753048e8d87c05934f9491500dff1a45b585
Ethan <ebrogames@gmail.com>
2023-04-02 01:57:02 -0400

    Create OxygenDepletedSolver.cs

---
+++ Neuro/Minigames/Completion/Solvers/OxygenDepletedSolver.cs
@@ -0,0 +1,29 @@
+﻿
+
+using Neuro.Cursor;
+using System;
+using System.Collections;
+
+namespace Neuro.Minigames.Completion.Solvers;
+
+[MinigameSolver(typeof(KeypadGame))]
+public sealed class OxygenDepletedSolver : MinigameSolver<KeypadGame>
+{
+    public override IEnumerator CompleteMinigame(KeypadGame minigame, NormalPlayerTask task)
+    {
+        int[] numbers = Array.ConvertAll(minigame.oxyTask.targetNumber.ToString().ToCharArray(), x => (int)char.GetNumericValue(x));
+        UiElement[] buttons = minigame.ControllerSelectable.ToArray();
+        foreach (int number in numbers)
+        {
+            // luckily for us the buttons are indexed correctly
+            if (number == 0)
+                yield return InGameCursor.Instance.CoMoveTo(buttons[9]);
+            else
+                yield return InGameCursor.Instance.CoMoveTo(buttons[number - 1]);
+            minigame.ClickNumber(number);
+            yield return Sleep(0.2f);
+        }
+        yield return InGameCursor.Instance.CoMoveTo(minigame.AcceptButton);
+        minigame.Enter();
+    }
+}
