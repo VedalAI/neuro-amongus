e69628c5e9fee56dbc4505a74957d339ab8d55df
Ethan <ebrogames@gmail.com>
2023-04-02 01:24:24 -0400

    Create RecordTemperatureSolver.cs

---
+++ Neuro/Minigames/Completion/Solvers/RecordTemperatureSolver.cs
@@ -0,0 +1,23 @@
+﻿using Neuro.Cursor;
+using System.Collections;
+using UnityEngine;
+
+namespace Neuro.Minigames.Completion.Solvers;
+
+[MinigameSolver(typeof(TempMinigame))]
+public sealed class RecordTemperatureSolver : MinigameSolver<TempMinigame>
+{
+    public override IEnumerator CompleteMinigame(TempMinigame minigame, NormalPlayerTask task)
+    {
+        // TODO: Figure out if the buttons are stored somewhere so we dont have to do this
+        int direction = minigame.logValue < minigame.readingValue ? 1 : -1;
+        Vector3 position = minigame.LogText.transform.position + new Vector3(0f, direction == 1 ? 0.7f : -0.7f, 0f);
+        yield return InGameCursor.Instance.CoMoveTo(position);
+        do
+        {
+            minigame.ChangeNumber(direction);
+            yield return Sleep(0.1f);
+        } while (minigame.logValue != minigame.readingValue);
+    }
+}
+
