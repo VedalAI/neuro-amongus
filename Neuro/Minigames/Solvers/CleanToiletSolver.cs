f867fb9883975f5b7cc6f879327d4aa384404e96
Ethan <ebrogames@gmail.com>
2023-04-06 02:31:08 -0400

    Create CleanToiletSolver.cs and EnterIdCodeSolver.cs

---
+++ Neuro/Minigames/Solvers/CleanToiletSolver.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using Neuro.Cursor;
+using UnityEngine;
+
+namespace Neuro.Minigames.Solvers;
+
+[MinigameSolver(typeof(ToiletMinigame))]
+public class CleanToiletSolver : MinigameSolver<ToiletMinigame>
+{
+    protected override IEnumerator CompleteMinigame(ToiletMinigame minigame, NormalPlayerTask task)
+    {
+        yield return InGameCursor.Instance.CoMoveTo(minigame.Plunger);
+        InGameCursor.Instance.StartHoldingLMB(minigame.Plunger);
+
+        while (!task.IsComplete)
+        {
+            yield return InGameCursor.Instance.CoMoveTo(minigame.Plunger.transform.position + new Vector3(0f, 1f, 0f), 2);
+            yield return Sleep(0.05f);
+            yield return InGameCursor.Instance.CoMoveTo(minigame.Plunger.transform.position + new Vector3(0f, -1f, 0f), 2);
+            yield return Sleep(0.05f);
+        }
+
+        InGameCursor.Instance.StopHolding();
+    }
+}
---
+++ Neuro/Minigames/Solvers/EnterIdCodeSolver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using Neuro.Cursor;
+
+namespace Neuro.Minigames.Solvers;
+
+[MinigameSolver(typeof(EnterCodeMinigame))]
+public class EnterIdCodeSolver : MinigameSolver<EnterCodeMinigame>
+{
+    protected override IEnumerator CompleteMinigame(EnterCodeMinigame minigame, NormalPlayerTask task)
+    {
+        yield return InGameCursor.Instance.CoMoveTo(minigame.Card);
+        minigame.ShowCard();
+
+        // the cardOut bool gets set immediately so give some time for it to appear
+        yield return Sleep(1.2f);
+        int[] numbers = Array.ConvertAll(minigame.targetNumber.ToString().ToCharArray(), x => (int)char.GetNumericValue(x));
+        UiElement[] buttons = minigame.ControllerSelectable.ToArray();
+        foreach (int number in numbers)
+        {
+            // luckily for us the buttons are indexed correctly
+            if (number == 0)
+                yield return InGameCursor.Instance.CoMoveTo(buttons[9]);
+            else
+                yield return InGameCursor.Instance.CoMoveTo(buttons[number - 1]);
+            minigame.EnterDigit(number);
+            yield return Sleep(0.2f);
+        }
+        yield return InGameCursor.Instance.CoMoveTo(buttons[11]);
+        
+        minigame.AcceptDigits();
+    }
+}
