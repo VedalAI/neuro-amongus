99189a08d1cef0f55ec6edcbac1adde22c90b4f0
Ethan <ebrogames@gmail.com>
2023-04-06 19:05:09 -0400

    Create SortSamplesSolver.cs

---
+++ Neuro/Minigames/Solvers/SortSamplesSolver.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using Neuro.Cursor;
+
+namespace Neuro.Minigames.Solvers;
+
+[MinigameSolver(typeof(SortMinigame))]
+public class SortSamplesSolver : MinigameSolver<SortMinigame>
+{
+    protected override IEnumerator CompleteMinigame(SortMinigame minigame, NormalPlayerTask task)
+    {
+        foreach (SortGameObject obj in minigame.Objects)
+        {
+            yield return InGameCursor.Instance.CoMoveTo(obj);
+            InGameCursor.Instance.StartHoldingLMB(obj);
+            switch (obj.MyType)
+            {
+                case SortGameObject.ObjType.Plant:
+                    yield return InGameCursor.Instance.CoMoveTo(minigame.PlantBox);
+                    break;
+                case SortGameObject.ObjType.Animal:
+                    yield return InGameCursor.Instance.CoMoveTo(minigame.AnimalBox);
+                    break;
+                case SortGameObject.ObjType.Mineral:
+                    yield return InGameCursor.Instance.CoMoveTo(minigame.MineralBox);
+                    break;
+            }
+            InGameCursor.Instance.StopHolding();
+            yield return Sleep(0.1f);
+        }
+    }
+}
