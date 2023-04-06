69f7b9f6ff8fdbeadf22205d56647a1e91c42335
Ethan <ebrogames@gmail.com>
2023-04-06 19:27:32 -0400

    Create StabilizeSteeringSkeldSolver.cs

---
+++ Neuro/Minigames/Solvers/StabilizeSteeringSkeldSolver.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using Neuro.Cursor;
+
+namespace Neuro.Minigames.Solvers;
+
+[MinigameSolver(typeof(NavigationMinigame))]
+public class StabilizeSteeringSkeldSolver : MinigameSolver<NavigationMinigame>
+{
+    protected override IEnumerator CompleteMinigame(NavigationMinigame minigame, NormalPlayerTask task)
+    {
+        InGameCursor.Instance.SnapToCenter();
+        InGameCursor.Instance.StartHoldingLMB(minigame.CrossHairImage);
+        yield return Sleep(0.5f);
+        InGameCursor.Instance.StopHolding();
+    }
+}
