4f0007c7c7729bc480744efd30c23449487d8e19
Ethan <ebrogames@gmail.com>
2023-04-07 18:53:13 -0400

    Create EmptyGarbageAirshipSolver.cs

---
+++ Neuro/Minigames/Solvers/EmptyGarbageAirshipSolver.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using Neuro.Cursor;
+using UnityEngine;
+
+namespace Neuro.Minigames.Solvers;
+
+[MinigameSolver(typeof(AirshipGarbageGame))]
+public class EmptyGarbageAirshipSolver : MinigameSolver<AirshipGarbageGame>
+{
+    protected override IEnumerator CompleteMinigame(AirshipGarbageGame minigame, NormalPlayerTask task)
+    {
+        yield return InGameCursor.Instance.CoMoveTo(minigame.can.Handle);
+        InGameCursor.Instance.StartHoldingLMB(minigame.can.Handle);
+        // yank that shit out of there
+        yield return InGameCursor.Instance.CoMoveTo(new Vector2(Screen.width / 2, Screen.height), 3f);
+        InGameCursor.Instance.StopHolding();
+    }
+}
