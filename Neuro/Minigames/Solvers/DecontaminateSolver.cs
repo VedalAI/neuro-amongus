54d291a9ebd826b96a175f8b347f9cc33ab6d859
Ethan <ebrogames@gmail.com>
2023-04-07 18:31:23 -0400

    Create DecontaminateSolver.cs

---
+++ Neuro/Minigames/Solvers/DecontaminateSolver.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+
+namespace Neuro.Minigames.Solvers;
+
+[MinigameSolver(typeof(ShowerMinigame))]
+public class DecontaminateSolver : MinigameSolver<ShowerMinigame>
+{
+    protected override IEnumerator CompleteMinigame(ShowerMinigame minigame, NormalPlayerTask task)
+    {
+        //       a programmer's worst nightmare       ^^^^^^^^^
+
+        // basically we just sit and wait for it to finish because we don't have to do anything
+        while (!task.IsComplete)
+            yield return null;
+    }
+}
