1f3a742c6d013ca3e8fb0778980e8cfe87e486f5
Ethan <ebrogames@gmail.com>
2023-04-07 18:23:24 -0400

    Create ResetBreakersSolver.cs

---
+++ Neuro/Minigames/Solvers/ResetBreakersSolver.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using Neuro.Cursor;
+using UnityEngine;
+
+namespace Neuro.Minigames.Solvers;
+
+[MinigameSolver(typeof(ElecLeverGame))]
+public class ResetBreakersSolver : MinigameSolver<ElecLeverGame>
+{
+    protected override IEnumerator CompleteMinigame(ElecLeverGame minigame, NormalPlayerTask task)
+    {
+        // TODO: programatically pathfind to the switches in order and/or remember ones we visit
+
+        if (minigame.MyNormTask.Data[minigame.MyNormTask.taskStep] == minigame.ConsoleId)
+        {
+            yield return InGameCursor.Instance.CoMoveTo(minigame.Handle);
+            InGameCursor.Instance.StartHoldingLMB(minigame.Handle);
+            Vector3 down = minigame.transform.TransformPoint(minigame.Handle.transform.localPosition + new Vector3(0f, -3f, 0f));
+            yield return InGameCursor.Instance.CoMoveTo(down);
+            yield return Sleep(0.1f);
+            InGameCursor.Instance.StopHolding();
+        }
+        else
+        {
+            yield return Sleep(0.5f);
+            minigame.Close();
+        }
+    }
+}
