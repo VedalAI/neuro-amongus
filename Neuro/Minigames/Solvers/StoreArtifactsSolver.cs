34b028f729a7f1fc4f4f0bb1558cf2f591c99b58
Ethan <ebrogames@gmail.com>
2023-04-07 17:27:26 -0400

    Create StoreArtifactsSolver.cs

---
+++ Neuro/Minigames/Solvers/StoreArtifactsSolver.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using Neuro.Cursor;
+
+namespace Neuro.Minigames.Solvers;
+
+[MinigameSolver(typeof(SpecimenGame))]
+public class StoreArtifactsSolver : MinigameSolver<SpecimenGame>
+{
+    protected override IEnumerator CompleteMinigame(SpecimenGame minigame, NormalPlayerTask task)
+    {
+        for (int i = 0; i < minigame.Specimens.Count; i++)
+        {
+            yield return InGameCursor.Instance.CoMoveTo(minigame.Specimens[i], 0.75f);
+            InGameCursor.Instance.StartHoldingLMB(minigame.Specimens[i]);
+            yield return InGameCursor.Instance.CoMoveTo(minigame.Slots[i], 0.75f);
+            yield return Sleep(0.1f);
+            InGameCursor.Instance.StopHolding();
+        }
+    }
+}
