c228d52cf7463d50436a18e538c814cdc688dfd0
Zero <knuklesetn@hotmail.com>
2023-04-07 23:52:38 +0200

    Create AirshipUploadSolver.cs

---
+++ Neuro/Minigames/Solvers/AirshipUploadSolver.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using Neuro.Cursor;
+
+namespace Neuro.Minigames.Solvers;
+
+[MinigameSolver(typeof(AirshipUploadGame))]
+public sealed class AirshipUploadSolver : MinigameSolver<AirshipUploadGame>
+{
+    protected override IEnumerator CompleteMinigame(AirshipUploadGame minigame, NormalPlayerTask task)
+    {
+        yield return InGameCursor.Instance.CoMoveTo(minigame.Phone);
+        minigame.phoneGrabbed = true;
+
+        yield return InGameCursor.Instance.CoMoveTo(minigame.Hotspot);
+        minigame.phoneGrabbed = false;
+    }
+}
