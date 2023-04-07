aa5101982ce0d5c554afbfdd15e007dbb8cb9081
Ethan <ebrogames@gmail.com>
2023-04-07 17:42:01 -0400

    Create RunDiagnosticsSolver.cs

---
+++ Neuro/Minigames/Solvers/RunDiagnosticsSolver.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using Neuro.Cursor;
+
+namespace Neuro.Minigames.Solvers;
+
+[MinigameSolver(typeof(DiagnosticGame))]
+public class RunDiagnosticsSolver : MinigameSolver<DiagnosticGame>
+{
+    protected override IEnumerator CompleteMinigame(DiagnosticGame minigame, NormalPlayerTask task)
+    {
+        if (task.TimerStarted == NormalPlayerTask.TimerState.NotStarted)
+        {
+            yield return InGameCursor.Instance.CoMoveTo(minigame.StartButton);
+            minigame.StartDiagnostic();
+            yield return Sleep(0.5f);
+            minigame.Close();
+        }
+        else if (task.TimerStarted == NormalPlayerTask.TimerState.Finished)
+        {
+            // TODO: make the cursor move to the dot instead of the center of the sprite
+            yield return InGameCursor.Instance.CoMoveTo(minigame.Targets[minigame.TargetNum]);
+            minigame.PickAnomaly(minigame.TargetNum);
+        }
+        else
+        {
+            yield return Sleep(0.5f);
+            minigame.Close();
+        }
+    }
+}
