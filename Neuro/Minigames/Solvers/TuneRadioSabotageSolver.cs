ecb7aeefc5525d43d4ec0d62b72198da23de9525
Zero <knuklesetn@hotmail.com>
2023-04-09 02:06:05 +0200

    Create TuneRadioSabotageSolver.cs

---
+++ Neuro/Minigames/Solvers/TuneRadioSabotageSolver.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Numerics;
+using HarmonyLib;
+using Neuro.Cursor;
+
+namespace Neuro.Minigames.Solvers;
+
+[MinigameSolver(typeof(TuneRadioMinigame))]
+public class TuneRadioSabotageSolver : MinigameSolver<TuneRadioMinigame>
+{
+    protected override IEnumerator CompleteMinigame(TuneRadioMinigame minigame, NormalPlayerTask task)
+    {
+        //TODO: Make this with InGameCursor and EulerAngles?
+        minigame.dial.SetValue(minigame.targetAngle);
+
+        yield return task;
+    }
+}
