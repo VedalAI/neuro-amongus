05f4c0ab507ccf5fba1fb1c2f4c44f40ef866330
ScrubN <72096833+ScrubN@users.noreply.github.com>
2023-04-08 21:37:12 -0400

    Add WeatherSwitchSolver.cs

---
+++ Neuro/Minigames/Solvers/WeatherSwitchSolver.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using Neuro.Cursor;
+using UnityEngine;
+
+namespace Neuro.Minigames.Solvers;
+
+[MinigameSolver(typeof(WeatherSwitchGame))]
+public class WeatherSwitchSolver : MinigameSolver<WeatherSwitchGame>
+{
+    protected override IEnumerator CompleteMinigame(WeatherSwitchGame minigame, NormalPlayerTask task)
+    {
+        var desiredSwitch = minigame.Controls[minigame.WeatherTask.NodeId];
+
+        yield return InGameCursor.Instance.CoMoveTo(desiredSwitch.Switch);
+        InGameCursor.Instance.StartHoldingLMB(desiredSwitch.Switch);
+        yield return new WaitForSeconds(0.1f);
+        InGameCursor.Instance.StopHolding();
+    }
+}
