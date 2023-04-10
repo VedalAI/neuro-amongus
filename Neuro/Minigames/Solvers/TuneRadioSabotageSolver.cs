8f26b211810c93eb70197ad48d1931d5c0ca74c6
Zero <knuklesetn@hotmail.com>
2023-04-10 17:39:39 +0200

    Use InGameCursor to solve TuneRadio

--- Neuro/Minigames/Solvers/TuneRadioSabotageSolver.cs
+++ Neuro/Minigames/Solvers/TuneRadioSabotageSolver.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
-using System.Numerics;
-using HarmonyLib;
 using Neuro.Cursor;
+using UnityEngine;
 
 namespace Neuro.Minigames.Solvers;
 
@@ -10,9 +9,40 @@ public class TuneRadioSabotageSolver : MinigameSolver<TuneRadioMinigame>
 {
     protected override IEnumerator CompleteMinigame(TuneRadioMinigame minigame, NormalPlayerTask task)
     {
-        //TODO: Make this with InGameCursor and EulerAngles?
-        minigame.dial.SetValue(minigame.targetAngle);
+        float radius = 0.6f;
+        float angle = 90f;
+        float angleStep = 1f;
 
-        yield return task;
+        Vector3 moveTo = PositionInCircumference(minigame.dial.DialTrans.transform.position, radius, angle);
+        yield return InGameCursor.Instance.CoMoveTo(moveTo);
+        InGameCursor.Instance.StartHoldingLMB(minigame.dial.DialTrans);
+
+        while (!minigame.finished)
+        {
+            if (minigame.targetAngle < minigame.dial.Value)
+            {
+                angle -= angleStep;
+            } else {
+                angle += angleStep;
+            }
+
+            moveTo = PositionInCircumference(minigame.dial.DialTrans.transform.position, radius, angle);
+            yield return InGameCursor.Instance.CoMoveTo(moveTo);
+        }
+
+        InGameCursor.Instance.StopHolding();
+    }
+
+    // https://answers.unity.com/questions/759542/get-coordinate-with-angle-and-distance.html
+    protected Vector3 PositionInCircumference(Vector3 center, float radius, float degrees)
+    {
+        float radians = degrees * Mathf.Deg2Rad;
+        float x = Mathf.Cos(radians);
+        float y = Mathf.Sin(radians);
+        Vector3 position = new Vector3(x, y, 0);
+        position *= radius;
+        position += center;
+
+        return position;
     }
 }
