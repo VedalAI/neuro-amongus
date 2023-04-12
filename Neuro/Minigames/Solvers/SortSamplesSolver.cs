c338eeabdefcdf839ad200ec92e0fa7e05e0e3ba
krogenth <kelleydaniel15@gmail.com>
2023-04-11 23:45:51 -0400

    implement artifact and sorting sample solvers

---
+++ Neuro/Minigames/Solvers/SortSamplesSolver.cs
@@ -0,0 +1,30 @@
+﻿using Neuro.Cursor;
+
+using System.Collections;
+
+namespace Neuro.Minigames.Solvers;
+
+[MinigameSolver(typeof(SortMinigame))]
+public sealed class SortSamplesSolver : MinigameSolver<SortMinigame>
+{
+    protected override IEnumerator CompleteMinigame(SortMinigame minigame, NormalPlayerTask task)
+    {
+        foreach (var obj in minigame.Objects)
+        {
+            yield return InGameCursor.Instance.CoMoveTo(obj);
+            InGameCursor.Instance.StartHoldingLMB(minigame);
+
+            switch (obj.MyType)
+            {
+                case SortGameObject.ObjType.Plant:
+                    yield return InGameCursor.Instance.CoMoveTo(minigame.PlantBox); break;
+                case SortGameObject.ObjType.Animal:
+                    yield return InGameCursor.Instance.CoMoveTo(minigame.AnimalBox); break;
+                case SortGameObject.ObjType.Mineral:
+                    yield return InGameCursor.Instance.CoMoveTo(minigame.MineralBox); break;
+            }
+
+            InGameCursor.Instance.StopHolding();
+        }
+    }
+}
