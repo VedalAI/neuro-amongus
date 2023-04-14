bad49e50c8f82b14e2fc93127eec52e317ffc3fc
AlexejheroYTB <32238504+Alexejhero@users.noreply.github.com>
2023-04-14 22:47:57 +0300

    Make HeaderFrame part of Frame

--- Neuro/Recording/Frame.cs
+++ Neuro/Recording/Frame.cs
@@ -1,4 +1,5 @@
 ﻿using Neuro.Recording.DeadBodies;
+using Neuro.Recording.Header;
 using Neuro.Recording.LocalPlayer;
 using Neuro.Recording.Map;
 using Neuro.Recording.OtherPlayers;
@@ -7,11 +8,15 @@ namespace Neuro.Recording;
 
 public partial class Frame
 {
-    public static Frame Now => new()
+    public static Frame Now(bool includeHeader = false)
     {
-        DeadBodies = DeadBodiesRecorder.Instance.Frame,
-        LocalPlayer = LocalPlayerRecorder.Instance.Frame,
-        Map = MapRecorder.Instance.Frame,
-        OtherPlayers = OtherPlayersRecorder.Instance.Frame
-    };
+        return new Frame
+        {
+            DeadBodies = DeadBodiesRecorder.Instance.Frame,
+            Header = includeHeader ? HeaderFrame.Generate() : null,
+            LocalPlayer = LocalPlayerRecorder.Instance.Frame,
+            Map = MapRecorder.Instance.Frame,
+            OtherPlayers = OtherPlayersRecorder.Instance.Frame
+        };
+    }
 }
