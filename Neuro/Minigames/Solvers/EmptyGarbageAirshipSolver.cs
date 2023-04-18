2e0150109465e38ebe4c07cca98084dea9de7f39
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2023-04-18 05:08:53 +0300

    Update EmptyGarbageAirshipSolver.cs

--- Neuro/Minigames/Solvers/EmptyGarbageAirshipSolver.cs
+++ Neuro/Minigames/Solvers/EmptyGarbageAirshipSolver.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 namespace Neuro.Minigames.Solvers;
 
-[MinigameSolver(typeof(AirshipGarbageGame))]
-public class EmptyGarbageAirshipSolver : GeneralMinigameSolver<AirshipGarbageGame>
+[MinigameSolver(typeof(AirshipGarbageGame), false)]
+[MinigameOpener(typeof(AutoMultistageMinigame))]
+public class EmptyGarbageAirshipSolver : IMinigameSolver<AirshipGarbageGame>, IMinigameOpener
 {
-    public override IEnumerator CompleteMinigame(AirshipGarbageGame minigame, NormalPlayerTask task)
+    public bool ShouldOpenConsole(Console console, Minigame minigame, PlayerTask task)
+    {
+        return task.TaskType == TaskTypes.EmptyGarbage;
+    }
+
+    public IEnumerator CompleteMinigame(AirshipGarbageGame minigame)
     {
         yield return InGameCursor.Instance.CoMoveTo(minigame.can.Handle);
-        InGameCursor.Instance.StartHoldingLMB(minigame.can.Handle);
+        InGameCursor.Instance.StartHoldingLMB(minigame);
         // yank that shit out of there
         yield return InGameCursor.Instance.CoMoveTo(new Vector2(Screen.width / 2, Screen.height), 3f);
         InGameCursor.Instance.StopHoldingLMB();
