146ca5c474ff1dd82d2ee596a62d1732a0770408
ScrubN <72096833+ScrubN@users.noreply.github.com>
2023-04-17 20:59:09 -0400

    Add FixWeatherNodeSolver.cs and Weather1Game_Patches.cs

---
+++ Neuro/Minigames/Solvers/FixWeatherNodeSolver.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using Il2CppSystem.Collections.Generic;
+using Neuro.Cursor;
+using Neuro.Minigames.Patches;
+using UnityEngine;
+
+namespace Neuro.Minigames.Solvers;
+
+[MinigameSolver(typeof(Weather1Game))]
+public class FixWeatherNodeSolver : GeneralMinigameSolver<Weather1Game>
+{
+    public override IEnumerator CompleteMinigame(Weather1Game minigame, NormalPlayerTask task)
+    {
+        HashSet<Vector3Int> solution = Weather1Game_SolveMaze.MazeSolution;
+
+        Vector3 tileBounds = minigame.fillTile.sprite.bounds.max * 2;
+        Vector3 mapPos = minigame.transform.position;
+
+        WaitForSeconds wait = new(0.04f);
+        foreach (Vector3Int tile in solution)
+        {
+            if (!minigame.PointIsValid(tile))
+            {
+                Error($"{tile} was invalid when it shouldn't be!");
+                continue;
+            }
+
+            var tileRealPosition = new Vector3((tile.x + 1) * tileBounds.x, (tile.y + 1) * tileBounds.y, (tile.z + 1) * tileBounds.z);
+            yield return InGameCursor.Instance.CoMoveTo(tileRealPosition + mapPos);
+
+            if (!InGameCursor.Instance.IsLeftButtonPressed) InGameCursor.Instance.StartHoldingLMB(minigame.transform);
+            yield return wait;
+        }
+
+        InGameCursor.Instance.StopHoldingLMB();
+    }
+}
