7ec4568a4ef24492e63b7e3d51745257d3d08f95
krogenth <kelleydaniel15@gmail.com>
2023-04-18 01:17:33 -0400

    fix start fans solver to avoid continuous use of first console

--- Neuro/Minigames/Solvers/StartFansSolver.cs
+++ Neuro/Minigames/Solvers/StartFansSolver.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 namespace Neuro.Minigames.Solvers;
 
 [MinigameSolver(typeof(StartFansMinigame))]
-public sealed class StartFansSolver : GeneralMinigameSolver<StartFansMinigame>
+public sealed class StartFansSolver : IMinigameSolver<StartFansMinigame, NormalPlayerTask>, IMinigameOpener<NormalPlayerTask>
 {
-    private static string[] codeSprites = new string[4];
+    private static Sprite[] codeSprites = new Sprite[4];
+    private static Console codeConsole;
 
-    public override IEnumerator CompleteMinigame(StartFansMinigame minigame, NormalPlayerTask task)
+    public bool ShouldOpenConsole(Console console, NormalPlayerTask task)
+    {
+        if (codeConsole == null || codeConsole != console) return true;
+        else return false;
+    }
+
+    public IEnumerator CompleteMinigame(StartFansMinigame minigame, NormalPlayerTask task)
     {
         if (task.TaskStep == 0) yield return CompleteStage1(minigame);
         else yield return CompleteStage2(minigame);
@@ -17,12 +24,14 @@ public sealed class StartFansSolver : GeneralMinigameSolver<StartFansMinigame>
 
     private IEnumerator CompleteStage1(StartFansMinigame minigame)
     {
+        // store console to avoid infinite loop
+        codeConsole = minigame.Console;
         yield return InGameCursor.Instance.CoMoveTo(minigame.mainCodeButton);
         minigame.RevealCode();
 
         for (int index = 0; index < minigame.CodeIcons.Count; index++)
         {
-            codeSprites[index] = minigame.CodeIcons[index].sprite.name;
+            codeSprites[index] = minigame.CodeIcons[index].sprite;
         }
 
         // fake committing code to memory
@@ -37,15 +46,14 @@ public sealed class StartFansSolver : GeneralMinigameSolver<StartFansMinigame>
         for (int index = 0; index < minigame.CodeIcons.Count; index++)
         {
             // check if this button is already set
-            if (minigame.CodeIcons[index].sprite.name == codeSprites[index]) continue;
+            if (minigame.CodeIcons[index].sprite == codeSprites[index]) continue;
 
             yield return InGameCursor.Instance.CoMoveTo(minigame.CodeIcons[index]);
-            while (minigame.CodeIcons[index].sprite.name != codeSprites[index])
+            while (minigame.CodeIcons[index].sprite != codeSprites[index])
             {
                 minigame.RotateImage(minigame.CodeIcons[index]);
-                yield return new WaitForSeconds(0.25f);
+                yield return new WaitForSeconds(0.33f);
             }
-            
         }
     }
 }
