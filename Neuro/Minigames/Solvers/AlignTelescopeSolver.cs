602347b5cf37a9346c4aca6a7dd241c3ab28568d
ScrubN <72096833+ScrubN@users.noreply.github.com>
2023-04-18 05:41:42 -0400

    Cleanup AlignTelescopeSolver.cs

--- Neuro/Minigames/Solvers/AlignTelescopeSolver.cs
+++ Neuro/Minigames/Solvers/AlignTelescopeSolver.cs
@@ -9,12 +9,10 @@ public sealed class AlignTelescopeSolver : GeneralMinigameSolver<TelescopeGame>
 {
     public override IEnumerator CompleteMinigame(TelescopeGame minigame, NormalPlayerTask task)
     {
-        Transform target = minigame.TargetItem.transform;
-
         yield return InGameCursor.Instance.CoMoveTo(minigame.Reticle);
 
         // Do a little square dance to be more realistic
-        InGameCursor.Instance.StartHoldingLMB(minigame.Reticle.transform);
+        InGameCursor.Instance.StartHoldingLMB(minigame);
         yield return InGameCursor.Instance.CoMoveTo(minigame.Reticle.transform.position + new Vector3(0, -3f));
         yield return new WaitForSeconds(0.3f);
 
@@ -33,19 +31,24 @@ public sealed class AlignTelescopeSolver : GeneralMinigameSolver<TelescopeGame>
         yield return wait;
         InGameCursor.Instance.StopHoldingLMB();
 
+        // Move towards the target in gradual steps
+        Transform target = minigame.TargetItem.transform;
         Vector3 positionDifference;
         do
         {
-            positionDifference = target.position - minigame.Reticle.transform.position;
+            positionDifference = minigame.Reticle.transform.position - target.position;
             yield return InGameCursor.Instance.CoMoveTo(minigame.Reticle);
-            InGameCursor.Instance.StartHoldingLMB(minigame.Reticle);
+            InGameCursor.Instance.StartHoldingLMB(minigame);
 
-            const int SPEED_MULTIPLIER = -8;
-            yield return InGameCursor.Instance.CoMoveTo(minigame.Reticle.transform.position + positionDifference.normalized * SPEED_MULTIPLIER);
+            const int DISTANCE_MULTIPLIER = 8;
+            yield return InGameCursor.Instance.CoMoveTo(minigame.Reticle.transform.position + positionDifference.normalized * DISTANCE_MULTIPLIER);
             InGameCursor.Instance.StopHoldingLMB();
-        } while (Mathf.Abs(positionDifference.x) + Mathf.Abs(positionDifference.y) > 2f);
+        } while (Mathf.Abs(positionDifference.x) + Mathf.Abs(positionDifference.y) > 2);
 
+        // Just in case we're a bit off, move exactly the remaining distance
         yield return InGameCursor.Instance.CoMoveTo(target);
+        InGameCursor.Instance.StartHoldingLMB(minigame);
         yield return InGameCursor.Instance.CoMoveTo(minigame.Reticle);
+        InGameCursor.Instance.StopHoldingLMB();
     }
 }
