b68c86f11a47efda146aa0a9290279acf4ddb1a7
ScrubN <72096833+ScrubN@users.noreply.github.com>
2023-04-17 22:30:13 -0400

    Add AssembleArtifactSolver.cs

---
+++ Neuro/Minigames/Solvers/AssembleArtifactSolver.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using Neuro.Cursor;
+using UnityEngine;
+
+namespace Neuro.Minigames.Solvers;
+
+[MinigameSolver(typeof(CrystalMinigame))]
+public class AssembleArtifactSolver : GeneralMinigameSolver<CrystalMinigame>
+{
+    public override IEnumerator CompleteMinigame(CrystalMinigame minigame, NormalPlayerTask task)
+    {
+        CrystalBehaviour[] crystals = minigame.CrystalPieces;
+        Transform[] crystalSlots = minigame.CrystalSlots;
+
+        WaitForSeconds wait = new(0.2f);
+        for (var i = 0; i < crystals.Length; i++)
+        {
+            yield return wait;
+            yield return InGameCursor.Instance.CoMoveTo(crystals[i]);
+            InGameCursor.Instance.StartHoldingLMB(minigame);
+            yield return InGameCursor.Instance.CoMoveTo(crystalSlots[i]);
+            InGameCursor.Instance.StopHoldingLMB();
+        }
+    }
+}
