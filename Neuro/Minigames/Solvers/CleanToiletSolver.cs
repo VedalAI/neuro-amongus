e76d8e32dc1ecbf68a036403d135b8b6aa6fcd9e
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2023-04-18 04:58:07 +0300

    (Code style changes) clean toilet

--- Neuro/Minigames/Solvers/CleanToiletSolver.cs
+++ Neuro/Minigames/Solvers/CleanToiletSolver.cs
@@ -10,13 +10,13 @@ public class CleanToiletSolver : GeneralMinigameSolver<ToiletMinigame>
     public override IEnumerator CompleteMinigame(ToiletMinigame minigame, NormalPlayerTask task)
     {
         yield return InGameCursor.Instance.CoMoveTo(minigame.Plunger);
-        InGameCursor.Instance.StartHoldingLMB(minigame.Plunger);
+        InGameCursor.Instance.StartHoldingLMB(minigame);
 
         while (!task.IsComplete)
         {
-            yield return InGameCursor.Instance.CoMoveTo(minigame.Plunger.transform.position + new Vector3(0f, 1f, 0f), 2);
+            yield return InGameCursor.Instance.CoMoveTo(minigame.Plunger.transform.position + new Vector3(0f, 1f), 2);
             yield return new WaitForSeconds(0.05f);
-            yield return InGameCursor.Instance.CoMoveTo(minigame.Plunger.transform.position + new Vector3(0f, -1f, 0f), 2);
+            yield return InGameCursor.Instance.CoMoveTo(minigame.Plunger.transform.position + new Vector3(0f, -1f), 2);
             yield return new WaitForSeconds(0.05f);
         }
 
