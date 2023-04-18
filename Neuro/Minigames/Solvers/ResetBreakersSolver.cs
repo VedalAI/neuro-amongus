d335e78fd84746654b01fa68c8dc619a79349e04
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2023-04-19 00:12:02 +0300

    Update reset breakers to work

--- Neuro/Minigames/Solvers/ResetBreakersSolver.cs
+++ Neuro/Minigames/Solvers/ResetBreakersSolver.cs
@@ -1,17 +1,32 @@
 ﻿using System.Collections;
+using System.Linq;
 using Neuro.Cursor;
 using UnityEngine;
 
 namespace Neuro.Minigames.Solvers;
 
 [MinigameSolver(typeof(ElecLeverGame))]
-public sealed class ResetBreakersSolver : GeneralMinigameSolver<ElecLeverGame>
+public sealed class ResetBreakersSolver : IMinigameSolver<ElecLeverGame, NormalPlayerTask>, IMinigameOpener<NormalPlayerTask>
 {
-    public override IEnumerator CompleteMinigame(ElecLeverGame minigame, NormalPlayerTask task)
+    public bool ShouldOpenConsole(Console console, NormalPlayerTask task)
     {
-        // TODO: programatically pathfind to the switches in order and/or remember ones we visit
+        // We use bytes 7-13 of task.Data to implement memory about which consoles have been seen
+        if (task.Data.Length == 7)
+        {
+            task.Data = task.Data.Concat(Enumerable.Repeat(byte.MaxValue, 7)).ToArray();
+        }
 
-        if (minigame.MyNormTask.Data[minigame.MyNormTask.taskStep] == minigame.ConsoleId)
+        // We want to open a console if we haven't seen it before or if we have seen it before and it's the next one in the sequence
+        int currentConsoleNumber = task.Data[console.ConsoleId + 7];
+        return currentConsoleNumber == byte.MaxValue || currentConsoleNumber == task.taskStep;
+    }
+
+    public IEnumerator CompleteMinigame(ElecLeverGame minigame, NormalPlayerTask task)
+    {
+        // Save console number to memory
+        task.Data[minigame.ConsoleId + 7] = (byte) task.Data.IndexOf((byte) minigame.ConsoleId);
+
+        if (task.Data[task.taskStep] == minigame.ConsoleId)
         {
             yield return InGameCursor.Instance.CoMoveTo(minigame.Handle);
             InGameCursor.Instance.StartHoldingLMB(minigame.Handle);
