d66ace668f3c89a69367ddf5f53cc3b6b0855a7e
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2023-04-18 05:03:40 +0300

    Update DressMannequinSolver.cs

--- Neuro/Minigames/Solvers/DressMannequinSolver.cs
+++ Neuro/Minigames/Solvers/DressMannequinSolver.cs
@@ -13,24 +13,26 @@ public class DressMannequinSolver : GeneralMinigameSolver<DressUpMinigame>
         // the hitbox placement in this minigame is terrible
         // innersloth pls fix
 
+        const float speed = 0.8f;
+
         DressUpCosmetic hat = minigame.buttons.First(b => b.Rend.sprite == minigame.DummyHat.sprite);
-        yield return InGameCursor.Instance.CoMoveTo(hat.transform.position + new Vector3(0f, 0.5f) , 0.75f);
-        InGameCursor.Instance.StartHoldingLMB(hat);
-        yield return InGameCursor.Instance.CoMoveTo(minigame.hatHitbox.bounds.center + new Vector3(0f, 0.3f), 0.5f);
+        yield return InGameCursor.Instance.CoMoveTo(hat.transform.position + new Vector3(0f, 0.5f), speed);
+        InGameCursor.Instance.StartHoldingLMB(minigame);
+        yield return InGameCursor.Instance.CoMoveTo(minigame.hatHitbox.bounds.center + new Vector3(0f, 0.3f), speed);
         yield return new WaitForSeconds(0.1f);
         InGameCursor.Instance.StopHoldingLMB();
 
         DressUpCosmetic accessory = minigame.buttons.First(b => b.Rend.sprite == minigame.DummyAccessory.sprite);
-        yield return InGameCursor.Instance.CoMoveTo(accessory, 0.75f);
-        InGameCursor.Instance.StartHoldingLMB(accessory);
-        yield return InGameCursor.Instance.CoMoveTo(minigame.faceHitbox, 0.5f);
+        yield return InGameCursor.Instance.CoMoveTo(accessory, speed);
+        InGameCursor.Instance.StartHoldingLMB(minigame);
+        yield return InGameCursor.Instance.CoMoveTo(minigame.faceHitbox, speed);
         yield return new WaitForSeconds(0.1f);
         InGameCursor.Instance.StopHoldingLMB();
 
         DressUpCosmetic clothes = minigame.buttons.First(b => b.Rend.sprite == minigame.DummyClothes.sprite);
-        yield return InGameCursor.Instance.CoMoveTo(clothes.transform.position - new Vector3(0f, 0.5f), 0.75f);
-        InGameCursor.Instance.StartHoldingLMB(clothes);
-        yield return InGameCursor.Instance.CoMoveTo(minigame.bodyHitbox.bounds.center, 0.5f);
+        yield return InGameCursor.Instance.CoMoveTo(clothes.transform.position - new Vector3(0f, 0.5f), speed);
+        InGameCursor.Instance.StartHoldingLMB(minigame);
+        yield return InGameCursor.Instance.CoMoveTo(minigame.bodyHitbox.bounds.center, speed);
         yield return new WaitForSeconds(0.1f);
         InGameCursor.Instance.StopHoldingLMB();
     }
