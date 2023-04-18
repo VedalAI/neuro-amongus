79f5a79a1cc9fefb1026b9fdf5c9e43c2d469953
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2023-04-19 00:35:32 +0300

    Update StoreArtifactsSolver.cs

--- Neuro/Minigames/Solvers/StoreArtifactsSolver.cs
+++ Neuro/Minigames/Solvers/StoreArtifactsSolver.cs
@@ -7,9 +7,12 @@ namespace Neuro.Minigames.Solvers;
 [MinigameSolver(typeof(SpecimenGame))]
 public sealed class StoreArtifactsSolver : GeneralMinigameSolver<SpecimenGame>
 {
-    // TODO: prevent unusual behaviour since the array and how they appear on screen are different
     public override IEnumerator CompleteMinigame(SpecimenGame minigame, NormalPlayerTask task)
     {
+        // Reorder artifacts so they are handled top-to-bottom
+        (minigame.Specimens[1], minigame.Specimens[2], minigame.Specimens[3]) = (minigame.Specimens[3], minigame.Specimens[2], minigame.Specimens[1]);
+        (minigame.Slots[1], minigame.Slots[2], minigame.Slots[3]) = (minigame.Slots[3], minigame.Slots[2], minigame.Slots[1]);
+
         for (int i = 0; i < minigame.Specimens.Count; i++)
         {
             yield return InGameCursor.Instance.CoMoveTo(minigame.Specimens[i], 0.75f);
