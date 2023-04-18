302c16129d98ea4075e13c5e2c16770e66e39b3d
krogenth <kelleydaniel15@gmail.com>
2023-04-18 10:35:50 -0400

    add med scan solver

---
+++ Neuro/Minigames/Solvers/SubmitScanSolver.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Neuro.Minigames.Solvers;
+
+[MinigameSolver(typeof(MedScanMinigame))]
+
+public sealed class SubmitScanSolver : GeneralMinigameSolver<MedScanMinigame>
+{
+    public override IEnumerator CompleteMinigame(MedScanMinigame minigame, NormalPlayerTask task)
+    {
+        yield return new WaitForSeconds(minigame.ScanDuration);
+    }
+}
