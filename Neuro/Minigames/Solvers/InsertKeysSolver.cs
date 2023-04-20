2772dfc758041a8e7d55779c1a8e92308c3e85eb
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2023-04-20 14:57:00 +0300

    Update InsertKeysSolver.cs

--- Neuro/Minigames/Solvers/InsertKeysSolver.cs
+++ Neuro/Minigames/Solvers/InsertKeysSolver.cs
@@ -10,19 +10,24 @@ public sealed class InsertKeysSolver : GeneralMinigameSolver<KeyMinigame>
     public override IEnumerator CompleteMinigame(KeyMinigame minigame, NormalPlayerTask task)
     {
         yield return InGameCursor.Instance.CoMoveTo(minigame.key);
-        InGameCursor.Instance.StartHoldingLMB(minigame.key);
+        InGameCursor.Instance.StartHoldingLMB(minigame);
         yield return InGameCursor.Instance.CoMoveTo(minigame.Slots[minigame.targetSlotId]);
         InGameCursor.Instance.StopHoldingLMB();
         yield return new WaitForSeconds(0.1f);
-        InGameCursor.Instance.StartHoldingLMB(minigame.key);
-        for (float t = 0; t < 0.5f; t += Time.deltaTime)
+
+        InGameCursor.Instance.StartHoldingLMB(minigame);
+        const float spinDuration = 0.2f;
+        for (float t = 0; t < spinDuration; t += Time.deltaTime)
         {
-            minigame.prevHadInput = true;
-            // TODO: make the cursor follow the topside of the key (its bounds are massive)
-            InGameCursor.Instance.SnapTo(minigame.key);
-            Vector3 currentAngles = minigame.key.transform.localEulerAngles;
-            currentAngles.z = Mathf.LerpAngle(0f, 90f, t / 0.5f);
-            minigame.key.transform.localEulerAngles = currentAngles;
+            Vector2 keyPosition = minigame.key.transform.position;
+
+            // This can also probably be done with some trigonometry but i'm too stupid for that
+            Vector2 positionOnChord = Vector2.Lerp(keyPosition + Vector2.up, keyPosition + Vector2.left, t / spinDuration);
+            Vector2 unnormalizedDirection = positionOnChord - keyPosition;
+            Vector2 normalizedDirection = unnormalizedDirection.normalized;
+            Vector2 positionOnCircle = keyPosition + normalizedDirection * 0.5f;
+
+            InGameCursor.Instance.SnapTo(positionOnCircle);
             yield return null;
         }
         InGameCursor.Instance.StopHoldingLMB();
