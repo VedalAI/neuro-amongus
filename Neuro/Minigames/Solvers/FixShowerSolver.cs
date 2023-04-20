99e6f163e946bc5d548fadb7c5d97b3a97e38706
krogenth <kelleydaniel15@gmail.com>
2023-04-20 13:58:42 -0400

    add fix shower solver

---
+++ Neuro/Minigames/Solvers/FixShowerSolver.cs
@@ -0,0 +1,26 @@
+﻿using Neuro.Cursor;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Neuro.Minigames.Solvers;
+
+[MinigameSolver(typeof(FixShowerMinigame))]
+public sealed class FixShowerSolver : GeneralMinigameSolver<FixShowerMinigame>
+{
+    public override IEnumerator CompleteMinigame(FixShowerMinigame minigame, NormalPlayerTask task)
+    {
+        do
+        {
+            yield return InGameCursor.Instance.CoMoveTo(minigame.showerHead);
+            InGameCursor.Instance.StartHoldingLMB(minigame.showerHead);
+            yield return new WaitForSeconds(Math.Abs(minigame.showerPos - 0.5f));
+            InGameCursor.Instance.StopHoldingLMB();
+            yield return new WaitForSeconds(0.5f);
+        } while (Math.Abs(minigame.showerPos - 0.55f) <= 0.5f);
+    }
+}
