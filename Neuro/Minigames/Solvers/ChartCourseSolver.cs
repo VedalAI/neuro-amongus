2a333a5d8941352da8d93e2a71a067b9738588f7
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2023-04-20 19:04:37 +0300

    Make chart course tiny bit faster

--- Neuro/Minigames/Solvers/ChartCourseSolver.cs
+++ Neuro/Minigames/Solvers/ChartCourseSolver.cs
@@ -12,22 +12,23 @@ public sealed class ChartCourseSolver : GeneralMinigameSolver<CourseMinigame>
         // for some reason this minigame uses localPositions for everything so we have to convert them into world positions
 
         yield return InGameCursor.Instance.CoMoveTo(minigame.Ship);
-        InGameCursor.Instance.StartHoldingLMB(minigame.Ship);
+        InGameCursor.Instance.StartHoldingLMB(minigame);
+
         int start = (int)minigame.Converter.GetFloat(minigame.MyNormTask.Data);
         for (int i = start; i < minigame.NumPoints; i++)
         {
             Vector3 point = minigame.PathPoints[i];
             Vector3 worldPos = minigame.transform.TransformPoint(point);
-            if (i + 1 == minigame.NumPoints)
+            if (i == minigame.NumPoints - 1)
             {
                 // minigame requires you to move the mouse slightly past the final point
                 // so we handle that here
                 worldPos += new Vector3(0.2f, 0.2f, 0f);
-                yield return InGameCursor.Instance.CoMoveTo(worldPos, 0.33f);
+                yield return InGameCursor.Instance.CoMoveTo(worldPos, 0.7f);
                 InGameCursor.Instance.StopHoldingLMB();
                 break;
             }
-            yield return InGameCursor.Instance.CoMoveTo(worldPos, 0.33f);
+            yield return InGameCursor.Instance.CoMoveTo(worldPos, 0.7f);
             yield return new WaitForSeconds(0.1f);
         }
     }
