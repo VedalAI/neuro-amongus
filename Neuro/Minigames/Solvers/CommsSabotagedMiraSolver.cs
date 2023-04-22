ee9ac796a5650cbc5b3531132836c1186d3060d2
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2023-04-22 04:49:34 +0300

    Make mira comms tiny bit faster

--- Neuro/Minigames/Solvers/CommsSabotagedMiraSolver.cs
+++ Neuro/Minigames/Solvers/CommsSabotagedMiraSolver.cs
@@ -1,5 +1,6 @@
 ﻿using Neuro.Cursor;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using Neuro.Utilities;
 using UnityEngine;
@@ -7,21 +8,21 @@ using UnityEngine;
 namespace Neuro.Minigames.Solvers;
 
 [MinigameSolver(typeof(AuthGame))]
-public sealed class BackupCodeSabotageSolver : IMinigameSolver<AuthGame, SabotageTask>, IMinigameOpener<SabotageTask>
+public sealed class CommsSabotagedMiraSolver : IMinigameSolver<AuthGame>, IMinigameOpener
 {
-    public bool ShouldOpenConsole(Console console, SabotageTask task) => true;
+    public bool ShouldOpenConsole(Console console, PlayerTask task) => true;
 
-    public IEnumerator CompleteMinigame(AuthGame minigame, SabotageTask task)
+    public IEnumerator CompleteMinigame(AuthGame minigame)
     {
         do
         {
             yield return new WaitForSeconds(0.5f);
-            var code = minigame.system.TargetNumber.ToString().Select(c => c - '0');
-            foreach (var number in code)
+            IEnumerable<int> code = minigame.system.TargetNumber.ToString().PadLeft(5, '0').Select(c => c - '0');
+            foreach (int number in code)
             {
                 yield return InGameCursor.Instance.CoMoveTo(minigame.ControllerSelectable.At(number > 0 ? number - 1 : 9));
                 minigame.ClickNumber(number);
-                yield return new WaitForSeconds(0.5f);
+                yield return new WaitForSeconds(0.25f);
             }
             yield return InGameCursor.Instance.CoMoveTo(minigame.ControllerSelectable.At(minigame.ControllerSelectable.Count - 1));
             minigame.Enter();
