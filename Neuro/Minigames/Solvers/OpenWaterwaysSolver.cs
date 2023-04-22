05c36d66351c5ce09cb2d738176bb5c42fda1bb1
ScrubN <72096833+ScrubN@users.noreply.github.com>
2023-04-21 22:20:35 -0400

    Simplify OpenWaterwaysSolver.cs with InGameCursor.CoMoveCircle

--- Neuro/Minigames/Solvers/OpenWaterwaysSolver.cs
+++ Neuro/Minigames/Solvers/OpenWaterwaysSolver.cs
@@ -11,19 +11,12 @@ public sealed class OpenWaterwaysSolver : GeneralMinigameSolver<WaterWheelGame>
     {
         Transform wheelTransform = minigame.Wheel.transform;
         Vector2 wheelPosition = wheelTransform.position;
-        Vector2 position = wheelPosition;
-        float time = 0f;
 
+        yield return InGameCursor.Instance.CoMoveToCircleStart(wheelPosition, 2f, 0);
+        InGameCursor.Instance.StartHoldingLMB(minigame);
         do
         {
-            time += Time.deltaTime;
-            yield return InGameCursor.Instance.CoMoveTo(position);
-
-            const int SPEED_MULTIPLIER = 80;
-            position.x = wheelPosition.x + Mathf.Sin(time * SPEED_MULTIPLIER) * -2.1f;
-            position.y = wheelPosition.y + Mathf.Cos(time * SPEED_MULTIPLIER) * 2.1f;
-
-            if (!InGameCursor.Instance.IsLeftButtonPressed) InGameCursor.Instance.StartHoldingLMB(minigame);
+            yield return InGameCursor.Instance.CoMoveCircle(wheelPosition, 2f, 0, 359.5f, 0.3f);
         } while (wheelTransform.localEulerAngles.z < 358.99f);
 
         InGameCursor.Instance.StopHoldingLMB();
