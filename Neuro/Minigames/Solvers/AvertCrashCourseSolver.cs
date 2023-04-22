bf0204d309bca26a800325cfae5d80a26fd07cca
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2023-04-22 04:43:46 +0300

    Make avert crash course a tiny bit faster

--- Neuro/Minigames/Solvers/AvertCrashCourseSolver.cs
+++ Neuro/Minigames/Solvers/AvertCrashCourseSolver.cs
@@ -1,5 +1,6 @@
 ﻿using Neuro.Cursor;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using Neuro.Utilities;
 using UnityEngine;
@@ -7,21 +8,21 @@ using UnityEngine;
 namespace Neuro.Minigames.Solvers;
 
 [MinigameSolver(typeof(AirshipAuthGame))]
-public sealed class AvertCrashCourseSabotageSolver : IMinigameSolver<AirshipAuthGame, SabotageTask>, IMinigameOpener<SabotageTask>
+public sealed class AvertCrashCourseSolver : IMinigameSolver<AirshipAuthGame>, IMinigameOpener
 {
-    public bool ShouldOpenConsole(Console console, SabotageTask task) => true;
+    public bool ShouldOpenConsole(Console console, PlayerTask task) => true;
 
-    public IEnumerator CompleteMinigame(AirshipAuthGame minigame, SabotageTask task)
+    public IEnumerator CompleteMinigame(AirshipAuthGame minigame)
     {
         do
         {
             yield return new WaitForSeconds(0.5f);
-            var code = minigame.system.TargetCode.ToString().Select(c => c - '0');
-            foreach (var number in code)
+            IEnumerable<int> code = minigame.system.TargetCode.ToString().PadLeft(5, '0').Select(c => c - '0');
+            foreach (int number in code)
             {
                 yield return InGameCursor.Instance.CoMoveTo(minigame.selectableButtons.At(number > 0 ? number - 1 : 9));
                 minigame.ClickNumber(number);
-                yield return new WaitForSeconds(0.5f);
+                yield return new WaitForSeconds(0.25f);
             }
             yield return InGameCursor.Instance.CoMoveTo(minigame.selectableButtons.At(minigame.selectableButtons.Count - 1));
             minigame.Enter();
