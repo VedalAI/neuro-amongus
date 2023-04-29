af5da8366e1232ef27bc658edf9c36f729b82c51
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2023-04-30 00:52:40 +0300

    Start updating upload data solver (not working rn)

--- Neuro/Minigames/Solvers/UploadDataAirshipSolver.cs
+++ Neuro/Minigames/Solvers/UploadDataAirshipSolver.cs
@@ -18,47 +18,73 @@ public sealed class UploadDataAirshipSolver : IMinigameSolver<AirshipUploadGame,
     {
         yield return InGameCursor.Instance.CoMoveTo(minigame.Phone);
         InGameCursor.Instance.StartHoldingLMB(minigame);
-        Vector2 target;
 
-        // before we know where poor is, just try random points on the screen
+        // Find poor zone
         bool poorFound = false;
         while (!poorFound)
         {
             Vector2 start = minigame.Phone.transform.position;
-            target = NeuroUtilities.MainCamera.ScreenToWorldPoint(new Vector2(Random.Range(10, Screen.width - 10), Random.Range(10, Screen.height - 10)));
+            Vector2 target = NeuroUtilities.MainCamera.ScreenToWorldPoint(new Vector2(Random.Range(10, Screen.width - 10), Random.Range(10, Screen.height - 10)));
             float time = Vector2.Distance(start, target) / 6f;
             for (float t = 0; t < time; t += Time.deltaTime)
             {
                 InGameCursor.Instance.SnapTo(Vector2.Lerp(start, target, t / time));
-                // if we find poor while moving to our target, break out early
                 if (minigame.Poor.IsTouching(minigame.Hotspot))
                 {
                     poorFound = true;
                     break;
                 }
+
                 yield return null;
+            }
+        }
+
+        Vector2 lastPosition = InGameCursor.Instance.transform.position;
+        float searchRadius = minigame.Good.bounds.extents.magnitude;
+
+        // Find good zone
+        while (!minigame.Good.IsTouching(minigame.Hotspot))
+        {
+            Vector2 target = (Vector2) InGameCursor.Instance.transform.position + Random.insideUnitCircle * searchRadius;
+            yield return InGameCursor.Instance.CoMoveTo(target, 0.2f);
+
+            // Check if the quality has decreased
+            if (!minigame.Poor.IsTouching(minigame.Hotspot))
+            {
+                // Go back to the last position
+                yield return InGameCursor.Instance.CoMoveTo(lastPosition, 0.2f);
+            }
+            else
+            {
+                lastPosition = InGameCursor.Instance.transform.position;
+                searchRadius += 0.1f; // Gradually increase the search radius
             }
         }
 
         yield return new WaitForSeconds(0.2f);
 
-        // afterward, good must be somewhere nearby, so just check in a radius near the hotspot
-        while (!minigame.Good.IsTouching(minigame.Hotspot))
-        {
-            target = (Vector2)minigame.Hotspot.transform.position + Random.insideUnitCircle * minigame.Good.bounds.extents;
-            yield return InGameCursor.Instance.CoMoveTo(target, 0.2f);
-        }
+        lastPosition = InGameCursor.Instance.transform.position;
+        searchRadius = minigame.Perfect.bounds.extents.magnitude;
 
-        yield return new WaitForSeconds(0.2f);
-
-        // do the same for perfect
+        // Find perfect zone
         while (!minigame.Perfect.IsTouching(minigame.Hotspot))
         {
-            target = (Vector2)minigame.Hotspot.transform.position + Random.insideUnitCircle * minigame.Perfect.bounds.extents;
+            Vector2 target = (Vector2) InGameCursor.Instance.transform.position + Random.insideUnitCircle * searchRadius;
             yield return InGameCursor.Instance.CoMoveTo(target, 0.1f);
+
+            // Check if the quality has decreased
+            if (!minigame.Good.IsTouching(minigame.Hotspot))
+            {
+                // Go back to the last position
+                yield return InGameCursor.Instance.CoMoveTo(lastPosition, 0.1f);
+            }
+            else
+            {
+                lastPosition = InGameCursor.Instance.transform.position;
+                searchRadius += 0.05f; // Gradually increase the search radius
+            }
         }
 
         InGameCursor.Instance.StopHoldingLMB();
     }
-
 }
