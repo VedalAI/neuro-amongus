1e0959d40e215dd620db9a9dc1b65f84247057bf
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2023-04-30 03:00:02 +0300

    Fix task error

--- Neuro/Utilities/NeuroUtilities.cs
+++ Neuro/Utilities/NeuroUtilities.cs
@@ -40,7 +40,7 @@ public static class NeuroUtilities
 
     public static List<Console> GetOpenableConsoles(bool includeSabotages)
     {
-        return PlayerControl.LocalPlayer.myTasks._items.Where(t => !t.IsComplete && !t.TryCast<ImportantTextTask>())
+        return PlayerControl.LocalPlayer.myTasks._items.Where(t => t && !t.IsComplete && !t.TryCast<ImportantTextTask>())
             .Where(t => includeSabotages || !PlayerTask.TaskIsEmergency(t))
             .SelectMany(t => t.FindConsoles()._items.Where(c => c).Select(c => (t, c)))
             .Where(e => MinigameHandler.ShouldOpenConsole(e.c, e.t.MinigamePrefab, e.t))
