91dc2febaeb0bd4345d362c9bcae3ce1f8cbf1af
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2023-05-01 04:39:23 +0300

    Why no worky

--- Neuro/Minigames/Solvers/UploadDataAirshipSolver.cs
+++ Neuro/Minigames/Solvers/UploadDataAirshipSolver.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using Neuro.Cursor;
 using Neuro.Utilities;
+using Reactor.Utilities.Extensions;
 
 namespace Neuro.Minigames.Solvers;
 
@@ -19,69 +21,109 @@ public sealed class UploadDataAirshipSolver : IMinigameSolver<AirshipUploadGame,
         yield return InGameCursor.Instance.CoMoveTo(minigame.Phone);
         InGameCursor.Instance.StartHoldingLMB(minigame);
 
-        // Find poor zone
-        bool poorFound = false;
-        while (!poorFound)
+        Dictionary<Vector2, GameObject> things = new();
+        HashSet<Vector2> positionsToSearch = new HashSet<Vector2>();
+        for (int x = 0; x < Screen.width; x += 50)
         {
-            Vector2 start = minigame.Phone.transform.position;
-            Vector2 target = NeuroUtilities.MainCamera.ScreenToWorldPoint(new Vector2(Random.Range(10, Screen.width - 10), Random.Range(10, Screen.height - 10)));
-            float time = Vector2.Distance(start, target) / 6f;
+            for (int y = 0; y < Screen.height; y += 50)
+            {
+                Vector2 position = NeuroUtilities.MainCamera.ScreenToWorldPoint(new Vector2(x, y));
+                positionsToSearch.Add(position);
+
+                GameObject nodeVisualPoint = new("Gizmo (Visual Point)")
+                {
+                    transform =
+                    {
+                        parent = minigame.transform,
+                        position = position
+                    }
+                };
+                nodeVisualPoint.transform.localPosition = nodeVisualPoint.transform.localPosition with {z = -20};
+
+                LineRenderer renderer = nodeVisualPoint.AddComponent<LineRenderer>();
+                renderer.SetPosition(0, nodeVisualPoint.transform.position);
+                renderer.SetPosition(1, nodeVisualPoint.transform.position + new Vector3(0, 0.2f));
+                renderer.widthMultiplier = 0.2f;
+                renderer.positionCount = 2;
+                renderer.material = NeuroUtilities.MaskShaderMat;
+                renderer.startColor = Color.red;
+                renderer.endColor = Color.red;
+
+                things[position] = nodeVisualPoint;
+            }
+        }
+
+        while (!minigame.Perfect.IsTouching(minigame.Hotspot))
+        {
+            // Pick a random position that can have an upgrading signal
+            Vector2 target = positionsToSearch.Random();
+            if (target == Vector2.zero)
+            {
+                yield return CompleteMinigame(minigame, task);
+                yield break;
+            }
+
+            // Move to the target position
+            float distance = Vector2.Distance(minigame.Phone.transform.position, target);
+            float time = distance / 6f;
+
+            Vector2 lastPosition = minigame.Phone.transform.position;
+            bool isPoor = minigame.Poor.IsTouching(minigame.Hotspot);
+            bool isGood = minigame.Good.IsTouching(minigame.Hotspot);
+
+            Vector2 originalPosition = minigame.Phone.transform.position;
             for (float t = 0; t < time; t += Time.deltaTime)
             {
-                InGameCursor.Instance.SnapTo(Vector2.Lerp(start, target, t / time));
-                if (minigame.Poor.IsTouching(minigame.Hotspot))
+                InGameCursor.Instance.SnapTo(Vector2.Lerp(originalPosition, target, t / time));
+
+                bool isDowngrade = (isPoor && !minigame.Poor.IsTouching(minigame.Hotspot)) ||
+                                   (isGood && !minigame.Good.IsTouching(minigame.Hotspot));
+                if (isDowngrade)
                 {
-                    poorFound = true;
+                    for (;; t -= Time.deltaTime)
+                    {
+                        InGameCursor.Instance.SnapTo(Vector2.Lerp(originalPosition, target, t / time));
+
+                        isDowngrade = (isPoor && !minigame.Poor.IsTouching(minigame.Hotspot)) ||
+                                           (isGood && !minigame.Good.IsTouching(minigame.Hotspot));
+                        if (!isDowngrade)
+                        {
+                            break;
+                        }
+
+                        yield return null;
+                    }
+                }
+
+                // Remove positions based on the current signal
+                positionsToSearch.RemoveWhere(p =>
+                {
+                    float dist = Vector2.Distance(minigame.Phone.transform.position, p);
+                    if (!minigame.Poor.IsTouching(minigame.Hotspot) && minigame.Poor.bounds.Contains(p))
+                    {
+                        things[p].Destroy();
+                        return true;
+                    }
+                    if (minigame.Poor.IsTouching(minigame.Hotspot) && !minigame.Poor.bounds.Contains(p))
+                    {
+                        things[p].Destroy();
+                        return true;
+                    }
+                    if (minigame.Good.IsTouching(minigame.Hotspot) && !minigame.Good.bounds.Contains(p))
+                    {
+                        things[p].Destroy();
+                        return true;
+                    }
+                    return false;
+                });
+
+                // If we have reached Perfect, break the loop and stop the function
+                if (minigame.Perfect.IsTouching(minigame.Hotspot))
+                {
                     break;
                 }
 
                 yield return null;
-            }
-        }
-
-        Vector2 lastPosition = InGameCursor.Instance.transform.position;
-        float searchRadius = minigame.Good.bounds.extents.magnitude;
-
-        // Find good zone
-        while (!minigame.Good.IsTouching(minigame.Hotspot))
-        {
-            Vector2 target = (Vector2) InGameCursor.Instance.transform.position + Random.insideUnitCircle * searchRadius;
-            yield return InGameCursor.Instance.CoMoveTo(target, 0.2f);
-
-            // Check if the quality has decreased
-            if (!minigame.Poor.IsTouching(minigame.Hotspot))
-            {
-                // Go back to the last position
-                yield return InGameCursor.Instance.CoMoveTo(lastPosition, 0.2f);
-            }
-            else
-            {
-                lastPosition = InGameCursor.Instance.transform.position;
-                searchRadius += 0.1f; // Gradually increase the search radius
-            }
-        }
-
-        yield return new WaitForSeconds(0.2f);
-
-        lastPosition = InGameCursor.Instance.transform.position;
-        searchRadius = minigame.Perfect.bounds.extents.magnitude;
-
-        // Find perfect zone
-        while (!minigame.Perfect.IsTouching(minigame.Hotspot))
-        {
-            Vector2 target = (Vector2) InGameCursor.Instance.transform.position + Random.insideUnitCircle * searchRadius;
-            yield return InGameCursor.Instance.CoMoveTo(target, 0.1f);
-
-            // Check if the quality has decreased
-            if (!minigame.Good.IsTouching(minigame.Hotspot))
-            {
-                // Go back to the last position
-                yield return InGameCursor.Instance.CoMoveTo(lastPosition, 0.1f);
-            }
-            else
-            {
-                lastPosition = InGameCursor.Instance.transform.position;
-                searchRadius += 0.05f; // Gradually increase the search radius
             }
         }
 
