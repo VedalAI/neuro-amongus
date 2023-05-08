c385b3e55727bd25cf2a00e4bc416eff32a5780f
Vedal <vedal@vedal.xyz>
2023-05-08 15:09:25 +0100

    Add file uploading to test server

--- Neuro/Recording/Recorder.cs
+++ Neuro/Recording/Recorder.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.IO;
+//using System.Collections.Generic;
+using System.Security.Cryptography;  
 using Google.Protobuf;
 using Il2CppInterop.Runtime.Attributes;
+using System.Collections;
 using Neuro.Communication.AmongUsAI;
 using Neuro.Events;
 using Neuro.Utilities;
 using Reactor.Utilities.Attributes;
 using UnityEngine;
+using BepInEx.Unity.IL2CPP.Utils;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
 
 namespace Neuro.Recording;
 
@@ -20,6 +28,8 @@ public sealed class Recorder : MonoBehaviour
 
     private int _fixedUpdateCalls;
     private FileStream _fileStream;
+
+    public string fileServerURL = "http://localhost:5000/";
 
     private void Awake()
     {
@@ -47,7 +57,7 @@ public sealed class Recorder : MonoBehaviour
         // TODO: We should record meeting data!
         if (MeetingHud.Instance || Minigame.Instance || PlayerControl.LocalPlayer.Data.IsDead) return;
 
-        if (CommunicationHandler.Instance.IsConnected)
+        if (CommunicationHandler.Instance && CommunicationHandler.Instance.IsConnected)
         {
             Warning("Connected to socket, stopping Recorder");
             Destroy(this);
@@ -67,6 +77,56 @@ public sealed class Recorder : MonoBehaviour
     private void OnDestroy()
     {
         _fileStream.Dispose();
+
+        // Send file to server
+        AmongUsClient.Instance.StartCoroutine(SendFileToServer());
+    }
+
+    IEnumerator SendFileToServer()
+    {
+        HttpClient client = new();
+
+        // Get request
+        Task<HttpResponseMessage> getTask = client.GetAsync(fileServerURL);
+        while (!getTask.IsCompleted && !getTask.IsCanceled && !getTask.IsFaulted && !getTask.IsCompletedSuccessfully) yield return null;
+        if (!getTask.IsCompletedSuccessfully)
+        {
+            Info("Could not fetch token.");
+            yield break;
+        }
+
+        // Get token
+        Task<string> result = getTask.Result.Content.ReadAsStringAsync();
+        while (!result.IsCompleted && !result.IsCanceled && !result.IsFaulted && !result.IsCompletedSuccessfully) yield return null;
+        string token = result.Result;
+        Info(token);
+
+        // SHA256 hash of token
+        SHA256 sha256 = SHA256.Create();
+        byte[] hash = sha256.ComputeHash(System.Text.Encoding.UTF8.GetBytes(token));
+        string hashString = BitConverter.ToString(hash).Replace("-", "").ToLower();
+
+        byte[] fileBytes = File.ReadAllBytes(_fileStream.Name);
+
+        // add hash to header
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", hashString);
+
+        // send file as multipart form data
+        MultipartFormDataContent form = new();
+
+        // Get file name from _fileStream.Name
+        string fileName = Path.GetFileName(_fileStream.Name);
+
+        form.Add(new ByteArrayContent(fileBytes), "file", fileName);
+        
+        Task<HttpResponseMessage> task = client.PostAsync(fileServerURL, form);
+        while (!task.IsCompleted && !task.IsCanceled && !task.IsFaulted && !task.IsCompletedSuccessfully) yield return null;
+        if (!task.IsCompletedSuccessfully)
+        {
+            Info("Could not send logs to the server.'");
+        }  else {
+            Info("Logs sent to the server.");
+        }
     }
 
     [HideFromIl2Cpp]
