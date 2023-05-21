b51f95b95d9b61b318abe8113776ac6201ddc1e5
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2023-05-21 09:43:41 +0300

    Re-add not banned patch

--- Neuro/Debugging/Patches/StatsManager_Patches.cs
+++ Neuro/Debugging/Patches/StatsManager_Patches.cs
@@ -2,7 +2,6 @@ using HarmonyLib;
 
 namespace Neuro.Debugging.Patches;
 
-#if FULL
 [HarmonyPatch(typeof(StatsManager), nameof(StatsManager.AmBanned), MethodType.Getter)]
 public static class StatsManager_get_AmBanned_Patch
 {
@@ -12,4 +11,3 @@ public static class StatsManager_get_AmBanned_Patch
         __result = false;
     }
 }
-#endif
