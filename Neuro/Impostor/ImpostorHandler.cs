0cdb82adc32cd0d7c75a2bc32a10139861401d58
Ethan <ebrogames@gmail.com>
2023-06-13 23:57:57 -0400

    cleanup and improve old venting code

---
+++ Neuro/Impostor/ImpostorHandler.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using Reactor.Utilities.Attributes;
+using System;
+using System.Collections;
+using System.Linq;
+using Neuro.Recording;
+using Il2CppInterop.Runtime.Attributes;
+using BepInEx.Unity.IL2CPP.Utils;
+using Neuro.Utilities;
+using Neuro.Events;
+
+namespace Neuro.Impostor;
+
+[RegisterInIl2Cpp, ShipStatusComponent]
+public sealed class ImpostorHandler : MonoBehaviour
+{
+    public ImpostorHandler(IntPtr ptr) : base(ptr) { }
+
+    public static ImpostorHandler Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance)
+        {
+            NeuroUtilities.WarnDoubleSingletonInstance();
+            Destroy(this);
+            return;
+        }
+
+        Instance = this;
+    }
+
+    [HideFromIl2Cpp]
+    public void HandleVent(int id)
+    {
+        Vent vent = ShipStatus.Instance.AllVents.First(v => v.Id == id);
+        this.StartCoroutine(CoStartVentOut(vent));
+    }
+
+    [HideFromIl2Cpp]
+    private IEnumerator CoStartVentOut(Vent original)
+    {
+        Vent[] possibleVents = GetAvailableNearbyVents(original);
+        Vent current = possibleVents[UnityEngine.Random.RandomRangeInt(0, possibleVents.Length)];
+        yield return new WaitForSeconds(UnityEngine.Random.RandomRange(0.8f, 1.2f));
+        if (!original.TryMoveToVent(current, out string error))
+        {
+            Error($"Failed to move to vent {current.Id}, reason: {error}");
+        }
+        while (true)
+        {
+            bool playerSpotted = false;
+            yield return new WaitForSeconds(UnityEngine.Random.RandomRange(0.8f, 1.2f));
+            Frame now = Frame.Now(true);
+            foreach (var player in now.OtherPlayers.LastSeenPlayers)
+            {           
+                // if we see a crewmate in our radius, try a different vent
+                if (player.IsVisible && !now.Header.OtherImpostors.Contains(player.Id))
+                {
+                    Info($"Spotted a crewmate, trying a different exit vent...");
+                    Vent next = GetAvailableNearbyVents(current)[UnityEngine.Random.RandomRangeInt(0, possibleVents.Length)];
+                    if (!current.TryMoveToVent(next, out error))
+                    {
+                        Error($"Failed to move to vent {next.Id}, reason: {error}");
+                    }
+                    current = next;
+                    playerSpotted = true;
+                    break;
+                }
+            }
+
+            if (!playerSpotted)
+            {
+                HudManager.Instance.ImpostorVentButton.DoClick();
+                yield break;
+            }
+        }
+    }
+
+    [HideFromIl2Cpp]
+    // since some vents can have a variable amount of neighbors, use this helper method to get available ones
+    private static Vent[] GetAvailableNearbyVents(Vent vent) => vent.NearbyVents.Where(v => v).ToArray();
+}
---
+++ Neuro/Impostor/Patches/PlayerPhysics_Patches.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+
+namespace Neuro.Impostor.Patches;
+
+[HarmonyPatch(typeof(PlayerPhysics), nameof(PlayerPhysics.RpcEnterVent))]
+public static class PlayerPhysics_RpcEnterVent
+{
+    [HarmonyPostfix]
+    public static void Postfix(int id)
+    {
+        ImpostorHandler.Instance.HandleVent(id);
+    }
+}
