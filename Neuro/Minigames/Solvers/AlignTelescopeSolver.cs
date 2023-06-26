dd217e0771c29e7cb561379bad9bcd2bd21f08bb
Ender Invader <enderinvader.end@gmail.com>
2023-06-26 16:52:56 -0700

    Create AlignTelescopeSolver.cs

---
+++ Neuro/Minigames/Solvers/AlignTelescopeSolver.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using Neuro.Cursor;
+using UnityEngine;
+
+namespace Neuro.Minigames.Solvers;
+
+[MinigameSolver(typeof(TelescopeGame))]
+public sealed class AlignTelescopeSolver : GeneralMinigameSolver<TelescopeGame>
+{
+    public override IEnumerator CompleteMinigame(TelescopeGame minigame, NormalPlayerTask task)
+    {
+        yield return InGameCursor.Instance.CoMoveTo(minigame.Reticle.transform.position);
+        InGameCursor.Instance.StartHoldingLMB(minigame);
+
+        // Add a bit of human
+        Vector2 rand = new Vector2(Random.RandomRange(-0.4f, 0.4f), Random.RandomRange(-0.4f, 0.4f));
+        float speed = Random.RandomRange(0.1f, 0.4f);
+
+        // Cursor controls background so it needs to be moved opposite of the target
+        yield return InGameCursor.Instance.CoMoveTo(InGameCursor.Instance.Position + -(Vector2)minigame.TargetItem.transform.localPosition + rand, speed);
+
+        InGameCursor.Instance.StopHoldingLMB();
+    }
+}
