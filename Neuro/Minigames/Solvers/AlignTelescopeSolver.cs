bc77bbb6f5b1fc2657e439be58389e5564b1a453
EnderInvader <enderinvader.end@gmail.com>
2023-06-28 11:12:26 -0700

    Added human-ness to telescope
    Randomly cycles through all items instead of directly going to target.

--- Neuro/Minigames/Solvers/AlignTelescopeSolver.cs
+++ Neuro/Minigames/Solvers/AlignTelescopeSolver.cs
@@ -12,12 +12,30 @@ public sealed class AlignTelescopeSolver : GeneralMinigameSolver<TelescopeGame>
         yield return InGameCursor.Instance.CoMoveTo(minigame.Reticle);
         InGameCursor.Instance.StartHoldingLMB(minigame);
 
-        // Add a bit of human
-        Vector2 rand = new Vector2(Random.RandomRange(-0.4f, 0.4f), Random.RandomRange(-0.4f, 0.4f));
-        float speed = Random.RandomRange(0.1f, 0.4f);
+        float speed = 0.2f;
 
-        // Cursor controls background so it needs to be moved opposite of the target
-        yield return InGameCursor.Instance.CoMoveTo(InGameCursor.Instance.Position + -(Vector2)minigame.TargetItem.transform.localPosition + rand, speed);
+        // Randomly order the targets
+        var targets = minigame.Items;
+        int count = targets.Count;
+        int last = count - 1;
+        for (var i = 0; i < last; ++i)
+        {
+            var r = Random.Range(i, count);
+            (targets[r], targets[i]) = (targets[i], targets[r]);
+        }
+
+        // Loop through the targets until minigame completes
+        while (!task.IsComplete)
+        {
+            Vector2 position = minigame.initialPos;
+            foreach (var target in targets)
+            {
+                // Cursor controls background so it needs to be moved opposite of the target
+                yield return InGameCursor.Instance.CoMoveTo(InGameCursor.Instance.Position + position - (Vector2)target.transform.localPosition, speed);
+                position = (Vector2)target.transform.localPosition;
+                yield return new WaitForSeconds(0.5f);
+            }
+        }
 
         InGameCursor.Instance.StopHoldingLMB();
     }
